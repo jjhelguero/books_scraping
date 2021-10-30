39163055cf0ba6baad65feea925a69d8685ec393
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2021-10-29 20:27:07 -0400

    Removed log:false option, added todo

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -1,20 +1,20 @@
 const hrefTitleSelector = 'h3 a'
 const productInformationTableSelector = '[class="table table-striped"]'
 
-
+//TODO: figure out cy-recurse implementation to refactor function
 const getPaginatedSearchResults = () => {
     //get random element on page to begin logic
-    cy.get('h1', {log:false})
+    cy.get('h1')
             .then( () => {
                 //iterate through each href selector to scrape book details and store them in json file, then navigate back one page
-                cy.get(hrefTitleSelector,{log:false})
+                cy.get(hrefTitleSelector)
                     .each(el => {
 
-                        cy.wrap(el,{log:false})
-                            .visit(el[0].href, {log:false})
+                        cy.wrap(el)
+                            .visit(el[0].href)
                             .then( () => {
                             
-                                cy.get('.default', {log:false})
+                                cy.get('.default')
                                 .map((components, index) => {
                                     let book = {
                                         title: Cypress.$('h1')[index].innerText.trim(),
@@ -25,27 +25,27 @@ const getPaginatedSearchResults = () => {
                                         availability: Cypress.$(`${productInformationTableSelector} tbody>tr:nth-child(6)>td`)[index].innerText.trim(),
                                         numberOfReviews: Cypress.$(`${productInformationTableSelector} tbody>tr:nth-child(7)>td`)[index].innerText.trim()
                                     }
-                                    cy.writeFile('booksSequentialArtAllDetails',book,{flag: 'a+', log:false})
+                                    cy.writeFile('booksSequentialArtAllDetails',book,{flag: 'a+'})
                                     return book
                                 })
                             })
-                            .go('back',{log:false})
+                            .go('back')
 
                         })
                 //Check if current page is last
-                cy.get('.current', {log:false})
+                cy.get('.current')
                         .then(el => {
                             const parts = el[0].innerText.split(' ')
                             const currentPage = parts[parts.length - 3]
                             const totalPages = parts[parts.length - 1]
 
                             if(parseInt(totalPages) > parseInt(currentPage)) {
-                                cy.get( 'a:contains("next")', {log:false})
+                                cy.get( 'a:contains("next")')
                                 .then(el => {
                                     //if so then click on button and rerun book detail scraping
                                     cy.log('Clicking next page results')
-                                    cy.wrap(el, {log:false})
-                                        .click({log:false})
+                                    cy.wrap(el)
+                                        .click()
                                     getPaginatedSearchResults()
                                 })
                             }//if not then exit scraping logic
@@ -64,7 +64,7 @@ const getPaginatedSearchResults = () => {
 it('It scrapes basic book data on landing page', () => {
     cy.visit('/')
 
-    cy.get('article',{log:false})
+    cy.get('article')
     .map((components, index) => {
         //extracts all book details and writes to file in json format
         let book = {
@@ -73,7 +73,7 @@ it('It scrapes basic book data on landing page', () => {
             availability: Cypress.$('.instock.availability')[index].innerText.trim(),
             starRating: Cypress.$('.star-rating')[index].classList[1].trim()
         }
-        cy.writeFile('booksBasicDetails',book,{flag: 'a+', log:false})
+        cy.writeFile('booksBasicDetails',book,{flag: 'a+'})
 
         return book
     })
